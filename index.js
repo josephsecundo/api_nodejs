9fd94584d038da9a012f697529dd97ab0e651d6e
Cleiton014 <159494633+Cleiton014@users.noreply.github.com>
2024-02-29 20:35:18 -0300

    criação da rota inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+// vem do proprio projeto
+const express = require('express')
+
+const app = express();
+
+// const porta = process.env.PORT || 3333;
+const porta = 3333;
+
+app.listen(porta, () => {
+    console.log("Servidor iniciado na porta " + porta);
+    // console.log(`Servidor iniciado na porta ${porta}`);
+});
+// request vem do font e response vem do back
+app.get("/", (request, response) => {
+    response.send("Olá mundo!");
+});
