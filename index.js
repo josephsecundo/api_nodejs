6a41789bb2f100b8048b7784fb5b9f655ed5da19
Cleiton014 <159494633+Cleiton014@users.noreply.github.com>
2024-03-05 19:34:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.listen(porta, () => {
     console.log("Servidor iniciado na porta " + porta);
     // console.log(`Servidor iniciado na porta ${porta}`);
 });
-// request vem do font e response vem do back
+// request vem do front e response vem do back
 app.get("/", (request, response) => {
     response.send("Olá mundo!");
 });
