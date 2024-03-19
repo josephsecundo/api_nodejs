16faef7d97b28e4165ae0a8b3b24c2e524959c86
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-03-19 20:30:10 -0300

    ROUTES ATT 2024_03_19

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,18 @@ const router = express.Router();
 
 // referência a controllers que serão utilizados nas rotas
 const UsuariosController = require('../controllers/usuarios'); 
+const ProdutosController = require('../controllers/produtos');
 
 
 router.get('/usuarios', UsuariosController.listarUsuarios); 
+router.post('/usuarios', UsuariosController.cadastrarUsuarios); 
+router.patch('/usuarios', UsuariosController.editarUsuarios); 
+router.delete('/usuarios', UsuariosController.apagarUsuarios); 
+
+router.get('/produtos', ProdutosController.listarProdutos); 
+router.post('/produtos', ProdutosController.cadastrarProdutos); 
+router.patch('/produtos', ProdutosController.editarProdutos); 
+router.delete('/produtos', ProdutosController.apagarProdutos); 
 
 
 module.exports = router;
